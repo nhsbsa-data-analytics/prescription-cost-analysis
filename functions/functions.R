6f3c1e982d5642d53494486af740bd7c279abd9b
GPlatten <32270079+GPlatten@users.noreply.github.com>
2023-04-11 10:10:46 +0100

    Set to source infobox functions

---
+++ functions/functions.R
@@ -0,0 +1,78 @@
+infoBox_border <- function(
+    header = "Header here",
+    text = "More text here",
+    backgroundColour = "#ccdff1",
+    borderColour = "#005EB8",
+    width = "31%",
+    fontColour = "black") {
+  
+  #set handling for when header is blank
+  display <- "block"
+  
+  if(header == "") {
+    display <- "none"
+  }
+  
+  paste(
+    "<div class='infobox_border' style = 'border: 1px solid ", borderColour,"!important;
+  border-left: 5px solid ", borderColour,"!important;
+  background-color: ", backgroundColour,"!important;
+  padding: 10px;
+  width: ", width,"!important;
+  display: inline-block;
+  vertical-align: top;
+  flex: 1;
+  height: 100%;'>
+  <h4 style = 'color: ", fontColour, ";
+  font-weight: bold;
+  font-size: 18px;
+  margin-top: 0px;
+  margin-bottom: 10px;
+  display: ", display,";'>", 
+  header, "</h4>
+  <p style = 'color: ", fontColour, ";
+  font-size: 16px;
+  margin-top: 0px;
+  margin-bottom: 0px;'>", text, "</p>
+</div>"
+  )
+}
+
+infoBox_no_border <- function(
+    header = "Header here",
+    text = "More text here",
+    backgroundColour = "#005EB8",
+    width = "31%",
+    fontColour = "white") {
+  
+  #set handling for when header is blank
+  display <- "block"
+  
+  if(header == "") {
+    display <- "none"
+  }
+  
+  paste(
+    "<div class='infobox_no_border',
+    style = 'background-color: ",backgroundColour,
+    "!important;padding: 10px;
+    margin-bottom: 20px;
+    width: ",width,";
+    display: inline-block;
+    vertical-align: top;
+    flex: 1;
+    height: 100%;'>
+  <h4 style = 'color: ", fontColour, ";
+  font-weight: bold;
+  font-size: 18px;
+  margin-top: 0px;
+  margin-bottom: 10px;
+  display: ", display,";'>", 
+  header, "</h4>
+  <p style = 'color: ", fontColour, ";
+  font-size: 16px;
+  margin-top: 0px;
+  margin-bottom: 0px;'>", text, "</p>
+</div>"
+  )
+}
--- pipeline.R
+++ pipeline.R
@@ -1,4 +1,10 @@
 ### Pipeline to run PCA annual publication
+# clear environment
+rm(list = ls())
+
+# source functions
+# this is only a temporary step until all functions are built into packages
+source("./functions/functions.R")
 
 # 1. Setup
 #install and library packages
@@ -7,4 +13,4 @@ install.packages("logr")
 library(logr)
 
 #set up logging
-lf <- log_open("Y:\\Official Stats\\PCA\\log\\pca_log.log")
+lf <- log_open("Y:/Official Stats/PCA/log/pca_log.log")
